9348071e724308a5076bcc0fa438199c2d55d758
Angga Sanjaya Lingga <aslingga@gmail.com>
2017-07-12 11:37:48 +0700

    initial import

---
+++ controller.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+app.controller('ctrl1', function($scope) {
+	$scope.name = 'Angga Sanjaya';
+});
+
+app.controller('ctrl2', function($scope) {
+    $scope.firstName = "Angga";
+    $scope.lastName = "Lingga";
+});
