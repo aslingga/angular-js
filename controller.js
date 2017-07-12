1970fb2f8a35c729a24285fc5e4a497b041e12ae
Angga Sanjaya Lingga <aslingga@gmail.com>
2017-07-12 13:02:48 +0700

    initial import

--- controller.js
+++ controller.js
@@ -2,11 +2,18 @@
  * 
  */
 
-app.controller('ctrl1', function($scope) {
-	$scope.name = 'Angga Sanjaya';
+app.controller("ctrl1", function($scope) {
+	$scope.name = "Angga Sanjaya";
 });
 
-app.controller('ctrl2', function($scope) {
+app.controller("ctrl2", function($scope) {
     $scope.firstName = "Angga";
     $scope.lastName = "Lingga";
+});
+
+app.directive("myDirective", function() {
+	return {
+		restrict: 'C',
+		template: 'My name is Angga Sanjaya Lingga'
+	}
 });
