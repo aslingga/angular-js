46db298694b517acbeb21087959e53ec9906413f
Angga Sanjaya Lingga <aslingga@gmail.com>
2017-07-20 09:48:33 +0700

    modifying view

--- app.js
+++ app.js
@@ -2,4 +2,5 @@
  * 
  */
 
+var url = "http://10.2.2.30:3010/api/Employees/profile/angga.sanjaya%40lkpp.go.id";
 var app = angular.module('app', []);
--- controller.js
+++ controller.js
@@ -9,11 +9,56 @@ app.controller("ctrl1", function($scope) {
 app.controller("ctrl2", function($scope) {
     $scope.firstName = "Angga";
     $scope.lastName = "Lingga";
+	$scope.fullName = function() {
+		return $scope.firstName + " " + $scope.lastName;
+	};
+
+	$scope.price = "50000";
 });
 
-app.directive("myDirective", function() {
+app.directive("eluneDirective1", function() {
 	return {
-		restrict: 'C',
-		template: 'My name is Angga Sanjaya Lingga'
+		restrict: "C",
+		template: "My name is Angga Sanjaya Lingga"
 	}
+});
+
+app.directive("eluneDirective2", function() {
+	return {
+		restrict: "E",
+		template: "Hello my name is Angga Sanjaya Lingga"
+	}
+});
+
+app.controller("ctrl3", function($scope) {
+    $scope.names = [
+        {name: "Angga Sanjaya", country: "Indonesia"},
+        {name: "Zin Yui", country: "China"},
+        {name: "Dominic Horton", country: "England"},
+		{name: "Margareth", country: "Scotland"},
+		{name: "Hege", country: "Norway"},
+		{name: "Joe", country: "Denmark"},
+		{name: "Gustav", country: "Sweden"},
+		{name: "Birgit", country: "Denmark"},
+		{name: "Mary", country: "Ireland"},
+		{name: "Kai", country: "Norway"}
+    ];
+
+	$scope.peopleOrderBy = function(x) {
+		$scope.peopleOrder = x;
+	}
+});
+
+app.run(function($rootScope) {
+	$rootScope.color = "Blue";
+});
+
+app.controller("ctrl4", function($scope) {
+	$scope.color = "Red";
+});
+
+app.controller("ctrl5", function($scope, $http) {
+	$http.get(url).then(function(response) {
+		$scope.email = response.data;
+	});
 });
